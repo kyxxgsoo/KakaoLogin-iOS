f099d6f4f2ddfdae750d45cbc1d302f38b47fa9d
KyungsooLee <jmtkd9196@gmail.com>
2022-11-12 20:10:20 +0900

    KakaoLoginTutorial project 생성

---
+++ KakaoLoginTutorial/KakaoLoginTutorial/KakaoLoginTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  KakaoLoginTutorialApp.swift
+//  KakaoLoginTutorial
+//
+//  Created by Kyungsoo Lee on 2022/11/12.
+//
+
+import SwiftUI
+
+@main
+struct KakaoLoginTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
