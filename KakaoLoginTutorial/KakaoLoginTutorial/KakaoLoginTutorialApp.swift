72ff3cbcc81fd673e421a670c0f74abadc302c6b
KyungsooLee <jmtkd9196@gmail.com>
2022-11-14 02:37:42 +0900

    iOS SDK 초기화

--- KakaoLoginTutorial/KakaoLoginTutorial/KakaoLoginTutorialApp.swift
+++ KakaoLoginTutorial/KakaoLoginTutorial/KakaoLoginTutorialApp.swift
@@ -6,9 +6,20 @@
 //
 
 import SwiftUI
+import KakaoSDKCommon
+import KakaoSDKAuth
 
 @main
 struct KakaoLoginTutorialApp: App {
+    
+    init() {
+        
+        let kakaoAppKey = Bundle.main.infoDictionary?["KAKAO_NATIVE_APP_KEY"] ?? ""
+        
+        // Kakao SDK 초기화
+        KakaoSDK.initSDK(appKey: kakaoAppKey as! String)
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
